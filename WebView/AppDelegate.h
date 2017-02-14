e2eb7d1d3b574f9653b824870f60a652c6cd0030
dlsuresh <dl.suresh6@gmail.com>
2017-02-14 17:25:16 +0530

    Initial Commit

---
+++ WebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebView
+//
+//  Created by Suresh on 2/14/17.
+//  Copyright © 2017 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebView
+//
+//  Created by Suresh on 2/14/17.
+//  Copyright © 2017 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
